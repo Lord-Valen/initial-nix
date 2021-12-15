4db8407901776d18ef0d1039cc77c2e31b9f9ee5
Lord-Valen <lord_valen@protonmail.com>
2021-12-15 14:33:20 -0500

    Flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      nixpkgs.follows = "nixpkgs";
+    };
+    nixt.url = "github:nix-community/nixt/typescript-rewrite";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      logos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux"; # the system architecture
+        modules = [
+            home-manager.nixosModules.home-manager
+            ./configuration.nix
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.lord-valen = import ./home.nix;
+            }
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
+  };
+}
